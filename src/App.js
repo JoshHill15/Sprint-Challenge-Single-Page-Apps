eef4b2df948ddf59cefae199fabcaaa7941ecc03
josh Hill <Josh_hill15@me.com>
2019-11-15 13:52:35 -0500

    mvp2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import Header from "./components/Header.js";
-import {Route, Link} from "react-router-dom"
+import {Route, Link, Switch} from "react-router-dom"
 import CharacterList from "./components/CharacterList.js";
 import WelcomePage from "./components/WelcomePage.js";
 import SearchForm from "./components/SearchForm.js";
+import SearchEpisode from "./components/SearchEpisode.js";
 
 export default function App() {
   return (
     <main >
         <div className = "header">
-          <Route exact to = "/" component = {Header} /> 
+          {/* <Route exact to = "/" component = {Header} />  */}
+          <Header />
           <WelcomePage />
         </div>
         <div className = "links">
           <Link className = "link"  to = "/">Home</Link><br/>
           <Link className = "link"  to = "/characters">Characters</Link><br/>
+          <Link className = "link"  to = "/episodes">Episodes</Link><br/>
+
         </div>
-          <Route  to = "/characters" component = {SearchForm} />
+          <Route exact to = "/characters" component = {SearchForm} />
+          <Route exact to = "/episodes" component = {SearchEpisode} />
           {/* <Route to = "/characters" component = {CharacterList} /> */}
     </main>
   );
--- src/components/SearchEpisode.js
+++ src/components/SearchEpisode.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../index.css"
-import SearchEpisode from "./SearchEpisode"
+import CharacterList from "./CharacterList";
 import {
   Card,
   CardImg,
@@ -11,31 +11,9 @@ import {
   CardSubtitle,
   Col
 } from "reactstrap";
-function SearchForm() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-
+function SearchEpisode() {
   const [searchEpisode, setSearchEpisode] = useState("");
   const [searchRes, setSearchRes] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://rickandmortyapi.com/api/character/")
-      .then(res => {
-        console.log(res.data.results);
-        // const result = res.data.results.filter(cv =>  {
-        //     cv.name.toLowerCase().includes(searchTerm.toLowerCase())
-        // })
-        const result = [];
-        res.data.results.forEach(cv => {
-          return cv.name.toLowerCase().includes(searchTerm.toLowerCase())
-            ? result.push(cv)
-            : null;
-        });
-        setSearchResults(result);
-      })
-      .catch(err => console.log("error:", err));
-  }, [searchTerm]);
-
   useEffect(() => {
     axios
       .get("https://rickandmortyapi.com/api/episode/")
@@ -56,32 +34,15 @@ function SearchForm() {
       .catch(err => console.log("error:", err));
   }, [searchEpisode]);
 
-
   // console.log(searchResults)
-  const handleChange = e => {
-    setSearchTerm(e.target.value);
+  const handleChanger= e => {
+    setSearchEpisode(e.target.value);
   };
-
-    const handleChanger = e => {
-      setSearchEpisode(e.target.value);
-    };
-  
 
   return (
     <section className="search-form">
       <form>
-        <div className = "search">
-          <label className = "search1" htmlFor="searchname">Search For Rick &amp; Morty Characters! </label>
-          <input
-            id="searchname"
-            type="text"
-            name="searchname"
-            placeholder="search"
-            onChange={handleChange}
-            value={searchTerm}
-            />
-        </div>
-        {/* <div className = "search">
+      <div className = "search">
           <label className = "search1" htmlFor="searchep">Search For Rick &amp; Morty Episodes! </label>
           <input
             id="searchep"
@@ -91,27 +52,26 @@ function SearchForm() {
             onChange={handleChanger}
             value={searchEpisode}
             />
-        </div> */}
+        </div>
       </form>
       <ul>
-        {searchResults.map((cv,index) => (
+        {searchRes.map((cv,index) => (
           <div>
             {/* <CharacterList key = {cv.id}/> */}
             {/* <p>{cv.name}</p> */}
             <div>
               <Col className = "col" sm = "6">
                 <Card className = "cards" key={index}>
-                  <CardImg top width="100%" src={cv.image} alt="Card image cap" />
                   <CardBody className="cardbody">
                     <ul>
                       <li>
                         <CardTitle>{cv.name}</CardTitle>
                       </li>
                       <li>
-                        <CardSubtitle>{cv.gender}</CardSubtitle>
+                        <CardSubtitle>{cv.episode}</CardSubtitle>
                       </li>
                       <li>
-                        <CardText>{cv.species}</CardText>
+                        <CardText>{cv.air_date}</CardText>
                       </li>
                     </ul>
                   </CardBody>
@@ -122,14 +82,6 @@ function SearchForm() {
         ))}
       </ul>
     </section>
-  );
+  )
 }
-
-export default SearchForm;
-
-//  function Search() {
-//     const [searchTerm, setSearchTerm] = useState("")
-//     const [searchResults, setSearchResults] = useState([])
-
-//   return (
-//     <section className="search-form">
+export default SearchEpisode
